060823125d95164cda5d9c58c324b49643ee09e5
rzmilon <rzmilon@gmail.com>
2022-12-14 21:18:45 +0600

    update

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { useState } from 'react';
 
 const AddUser = () => {
+    const [user, setUser] = useState({});
+    const handelAddUser = event=>{
+        event.preventDefault();
+        console.log(user);
+
+        fetch('http://localhost:5000/users',{
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.acknowledged){
+                alert('User added successfully');
+                event.target.reset();
+            }
+        })
+    };
+
+    const handelInputBlur = event =>{
+        const value = event.target.value;
+        const field = event.target.name;
+        const newUser = {...user};
+        newUser[field] = value;
+        setUser(newUser)
+    }
     return (
         <div>
             <h2>Please add a new user</h2>
+
+            <form onSubmit={handelAddUser}>
+                <input onBlur={handelInputBlur} type="text" name="name" placeholder='name' id="" required/>
+                <br />
+                <input onBlur={handelInputBlur} type="email" name="email" placeholder='email' id="" required/>
+                <br />
+                <input onBlur={handelInputBlur} type="text" name="address" id="" placeholder='address'/>
+                <br /> <br />
+                <button type="submit">Add User</button>
+            </form>
         </div>
     );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const users = useLoaderData();
+    const [displayUsers, setDisplayUsers] = useState(users);
+    
+    const handelDelete = user =>{
+        const agree = window.confirm(`Are you sure want to delete ${user.name}`);
+        console.log(agree);
+
+        if(agree){
+            // console.log('delete user with id', user._id);
+            fetch(`http://localhost:5000/users/${user._id}`,{
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if(data.deletedCount > 0)
+                alert('User deleted successfully');
+                const remainingUsers = displayUsers.filter(usr => usr._id !== user._id)
+
+            })
+        }
+        
+    }
     return (
         <div>
-            <h2>User: </h2>
+            <h2>User: {displayUsers.length}</h2>
+            <div>
+                {
+                    displayUsers.map(user => <p key={user._id}>
+                        {user.name} {user.email} <button onClick={()=>handelDelete(user)}>X</button>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
